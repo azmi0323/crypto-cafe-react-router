fa0832e91cb4946344ccd85bc8fc580fdcbceb81
AzmiHabib <azmihabib0323@gmail.com>
2022-03-30 21:03:44 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react'
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import About from "./components/About/About";
+import Coins from "./components/Coins/Coins";
+import Header from "./components/Header/Header";
+import Contact from "./components/Contact/Contact";
+import Homepage from './components/Homepage/Homepage'
 const App = () => {
   return (
     <div>
-      <h1>Crypto Cafe Starter Template</h1>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Homepage></Homepage>}></Route>
+        <Route path="/coins" element={<Coins></Coins>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
+        <Route path="*" element={<Coins></Coins>}></Route>
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
