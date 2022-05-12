f183ea99e183b10ed7b3daa7c9de3b2d247f617d
AzmiHabib <azmihabib0323@gmail.com>
2022-05-13 01:14:15 +0600

    edit address

--- src/components/Contact/BdAddress.js
+++ src/components/Contact/BdAddress.js
@@ -7,8 +7,8 @@ const BdAddress = () => {
         <div>
           <h1>Street: 17/4 Dhanmondi.</h1>
           <h1>City: Dhaka</h1>
-          <h1>Phone: +8801622222456</h1>
-          <h1>Email: contact.bd@cc.com</h1>
+          <h1>Phone: +8801303823282</h1>
+          <h1>Email: azmihabib@gmail.com</h1>
         </div>
         <div>
           <h1 className='text-2xl'>BANGLADESH OFFICE</h1>
